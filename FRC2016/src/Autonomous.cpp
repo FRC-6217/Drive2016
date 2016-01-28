fc7bdefaa280b4236e922786f657f7ee668651b3
ewilden2017 <ewilden2017@gmail.com>
2016-01-28 16:31:30 -0600

    Added functions for all defenses

--- FRC2016/src/Autonomous.cpp
+++ FRC2016/src/Autonomous.cpp
@@ -10,8 +10,32 @@ bool doNothing() {
 	return true;
 }
 
+//cross functions
 bool lowBar() {
 	return false;
 }
 
-
+bool portcullis() {
+	return false;
+}
+bool chevalDeFrise() {
+	return false;
+}
+bool moat() {
+	return false;
+}
+bool ramparts() {
+	return false;
+}
+bool drawBridge() {
+	return false;
+}
+bool sallyPort() {
+	return false;
+}
+bool rockWall() {
+	return false;
+}
+bool roughTerrain() {
+	return false;
+}
--- FRC2016/src/Autonomous.h
+++ FRC2016/src/Autonomous.h
@@ -4,11 +4,28 @@
 #include <map>
 
 bool doNothing();
+//cross functions
 bool lowBar();
+bool portcullis();
+bool chevalDeFrise();
+bool moat();
+bool ramparts();
+bool drawBridge();
+bool sallyPort();
+bool rockWall();
+bool roughTerrain();
 
 const std::map<std::string, bool (*)()> crossFunctions
 {
-	{"Low Bar", &lowBar}
+	{"Low Bar", &lowBar},
+	{"Portcullis", &portcullis},
+	{"Cheval De Frise", &chevalDeFrise},
+	{"Moat", &moat},
+	{"Ramparts", &ramparts},
+	{"Drawbridge", &drawBridge},
+	{"Sally Port", &sallyPort},
+	{"Rock Wall", &rockWall},
+	{"Rough Terrain", &roughTerrain}
 };
 
 #endif
